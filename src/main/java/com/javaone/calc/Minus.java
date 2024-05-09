16f4fdc4dfc2b5ed6c0614d4fd72ee253aa0bf1c
yerin <greyblanket@naver.com>
2024-05-09 15:10:04 +0900

    나머지 기능 (#5)

--- src/main/java/com/javaone/calc/Minus.java
+++
@@ -1,9 +0,0 @@
-package com.javaone.calc;
-
-public class Minus {
-
-    public int minusOneNumbers(int a, int b){
-
-        return a - b;
-    }
-}
---
+++ src/test/java/com/javaone/calc/Remaindertest.java
@@ -0,0 +1,29 @@
+package com.javaone.calc;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class Remaindertest {
+
+    @Test
+    @DisplayName("10을 2로 나누었을때 나머지가 나오는지 확인")
+    public void testRemainderTwoNum() {
+
+        Remainder remainder = new Remainder();
+        int res = remainder.remainderTwoNum(10,2);
+
+        Assertions.assertEquals(0,res);
+    }
+
+    @Test
+    @DisplayName("10 % 3 check")
+    public void testRemainderTwoNum2() {
+
+        Remainder remainder = new Remainder();
+        int res = remainder.remainderTwoNum(10,3);
+
+        Assertions.assertEquals(1,res);
+    }
+}
+
