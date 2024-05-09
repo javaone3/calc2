9ab21dcfa29b45a5796459bedca10faf837d8bc5
rreadytogo <alwaysreadytorunn@gmail.com>
2024-05-09 15:07:19 +0900

    곱하기 기능 추가

--- src/main/java/Ex.java
+++ src/main/java/Ex.java
@@ -1,2 +1,5 @@
 public class Ex {
+    public static void main(String[] args) {
+        System.out.println('f');
+    }
 }
--- src/test/java/testEx.java
+++ src/test/java/testEx.java
@@ -1,2 +1,5 @@
-public class testEx {
+public class Ex {
+    public static void main(String[] args) {
+        System.out.println('f');
+    }
 }
